895e841fa82819a5b863d18cb352a08d20b30a85
Vincent Rubingh <vincentrubingh@gmail.com>
2016-03-21 18:43:01 +1100

    Save and load list of photo info to/from external file

--- flikr_fetch.py
+++ flikr_fetch.py
@@ -2,10 +2,47 @@ import flickrapi
 import json
 import pickle
 
+save_file = "photos_db.p"
+
 api_key = u'736c046ac91ed5f2fe5330642e4b2db1'
 api_secret = u'cc5f4efa2cfb491e'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
+
+def initialize_save_file():
+    pickle_dictionary = {"enabled" : True}
+    with open(save_file, "wb") as f:
+        pickle.dump(pickle_dictionary, f)
+
+#
+#   Save a list of photo data to the save_file
+#   Items in the list are tuples of the form (id, owner)
+#
+def save_to_file(city, photo_list):
+    pickle_dictionary = None
+    with open(save_file, "rb") as f:
+        pickle_dictionary = pickle.load(f)
+
+    with open(save_file, "wb") as f:
+        pickle_dictionary[city] = photo_list
+        pickle.dump(pickle_dictionary, f)
+
+
+#
+#   Load and return a list of photo data from the save_file
+#   Items in the list are tuples of the form (id, owner)
+#
+def load_from_file(city):
+    pickle_dictionary = None
+    with open(save_file, "rb") as f:
+        pickle_dictionary = pickle.load(f)
+
+    if pickle_dictionary is not None:
+        if city in pickle_dictionary:
+            return pickle_dictionary[city]
+
+    print("Error: could not load city from file")
+    return None
 
 #
 #   Get the place id for any given query
@@ -41,51 +78,52 @@ def get_photos_from_place(place_id, latitude, longitude, debug=True):
                                      accuracy=12, 
                                      per_page=500)
 
-
     # first check total number of results
     if "photos" in json_item:
         json_item = json_item['photos']
         if int(json_item['total']) > int(json_item['perpage']):
             total_pages = json_item['pages']
             print("multiple pages (" + str(total_pages) + ")")
-            get_photos_from_place_page(place_id, latitude, longitude)
+            return get_photos_from_place_page(place_id, latitude, longitude)
         else:
-            print("single page")
+            print("Error: only one page returned, need more to create map")
         if debug: print ("perpage = " + str(json_item['perpage']))
 
     else:
         print("Error: could not resolve JSON")
 
+    return None
+
 
 def get_photos_from_place_page(place_id, latitude, longitude, page=1, debug=True):  
     json_item = flickr.photos.search(lat=latitude,lon=longitude, place_id=place_id,
                                      accuracy=12, 
-                                     per_page=500)    
-    photos_list = []
+                                     per_page=500,
+                                     page=page)    
+    photo_list = []
 
     if "photos" in json_item:
         photos = json_item['photos']
-        print("I'm in")
-        counter = 0
         for photo in photos['photo']:
-            photos_list.append((photo['id'], photo['owner']))
-        print(str(photos_list))
+            photo_list.append((photo['id'], photo['owner']))
+        #print(str(photo_list))
+        return photo_list
     else:
         print("Error: could not resolve JSON")
+        return None
 
 
 def print_json(json_item):
     print(json.dumps(json_item, indent=4))
 
 
-
-
 #first get place id
-place_id, latitude, longitude = place_id("sydney")
+#place_id, latitude, longitude = place_id("sydney")
 #print("place = " + str(place_id) + ", (lat, lon) = (" + str(latitude) + ", " + str(longitude) + ")")
-get_photos_from_place(place_id, latitude, longitude)
+#photo_list = get_photos_from_place(place_id, latitude, longitude)
 
-
-
+#save_to_file("sydney", photo_list)
+photo_list = load_from_file("sydney")
+print("photo_list = " + str(photo_list))
 
 
